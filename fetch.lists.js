13182cf9d6933652f6e1d990f1f10f38e5e90d2a
Adrian Rossouw <adrian@daemon.co.za>
2013-09-20 06:03:00 +0200

    semi-working worker script for tumblr.

---
+++ fetch.lists.js
@@ -0,0 +1,99 @@
+var nodeio  = require('node.io');
+var cheerio = require('cheerio');
+var debug = require('debug')('tumblr:fetch.lists');
+var async   = require('async');
+var path    = require('path');
+var url    = require('url');
+var _       = require('underscore');
+var db      = require('nano')('http://localhost:5984/tumblr');
+
+_.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
+
+var defaults = {
+    name: 'tof34',
+    count: 5,
+    start: 1
+};
+
+
+var options = {
+    timeout: 10,
+    max: 1,
+    retries: 3   
+};
+var tpl = _.template('http://{{name}}.tumblr.com/api/read?type=photo&num={{count}}&start={{start}}');
+
+exports.job = new nodeio.Job({
+    input: _.range(1, 5, 5),
+    run: function (num) {
+        var options = { start: num };
+        _.defaults(options, defaults);
+        var url = tpl(options);
+
+        this.get(url, function(err, data, headers) {
+            if (err) { return this.fail(err); }
+
+            var $ = cheerio.load(data);
+            var posts = $('tumblr post').map(mapJson(options, $));
+            debug('fetched post count', posts.length);
+
+            async.filter(posts, filterExisting, doEachSeries.bind(this));
+        });
+    }
+});
+
+function filterExisting(post, next) { db.head(post._id, next); }
+
+function doEachSeries(posts) {
+    debug('new post count', posts.length);
+    async.eachSeries(posts, doWaterfall, completed.bind(this)); 
+}
+
+function doWaterfall(post, next) {
+    debug('waterfall', post._id);
+    async.waterfall([
+        async.apply(saveDocument, post),
+        streamAttachment
+    ], next);
+}
+
+function saveDocument(post, next) {
+    debug('saveDocument', post._id);
+    db.insert(post, post._id, function(err, data) {
+        next(err, _.extend({}, post, data));
+    });
+}
+
+function streamAttachment(post, next) {
+    debug('streamAttachment', post._id, post.rev);
+    next('fail');
+}
+
+function completed(err, result) {
+    debug('completed', result);
+    if (err) { return this.fail(err); }
+    this.emit(result);
+}
+
+function mapJson(options, $) {
+    return function mapFn() {
+        var $el = $(this);
+
+        var img = $el.find('photo-url[max-width=1280]').text();
+        var tags = $el.find('tag').map(function() { return $(this).text(); });
+        var ext = path.extname(url.parse(img).pathname);
+        return {
+            _id: options.name + '+' + $el.attr('id'),
+            url: $el.attr('url'),
+            type: 'photo',
+            date: $el.attr('date-gmt'),
+            timestamp: $el.attr('unix-timestamp'),
+            width: $el.attr('width'),
+            height: $el.attr('height'),
+            image: img,
+            extension: ext.replace(/^\./,''),
+            tags: tags
+        };
+    };
+}
+
