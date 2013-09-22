2bf36650dbef185d0d8c7af9b5dfcdac29ddd3b3
Adrian Rossouw <adrian@daemon.co.za>
2013-09-22 04:27:43 +0200

    fix mime types.

--- couch-app/app.js
+++ couch-app/app.js
@@ -1,8 +1,11 @@
 var couchapp = require('couchapp'),
+    mime = require('couchapp/mimetypes'),
     path = require('path');
 
+mime.types.woff = 'application/x-font-woff';
+
 var ddoc = {
-  _id: '_design/tumblr-browsr',
+  _id: '_design/app',
   rewrites: require('./rewrites.json'),
   views: {},
   lists: {},
