2d10eec927f8c7d56962596294af4c4299a5b2b5
Adrian Rossouw <adrian@daemon.co.za>
2013-09-23 02:12:41 +0200

    more improvements

--- couch-app/src/js/app.js
+++ couch-app/src/js/app.js
@@ -14,62 +14,30 @@ angular.module('tumblr-browsr', ['CornerCouch', 'wu.masonry', 'infinite-scroll',
     .directive('magnificPopup', function() {
         return {
             restrict: 'A',
-            link: function(scope, element, attrs) {
-                scope.$watch('rows', function() {
-                    //var items = _(scope.rows).map(mapItems);
-
-                    $(element).magnificPopup({
-                        type: 'image',
-                        verticalFit: false,
-                        delegate: 'a',
-                        preload: [1,2],
-                        //items: items,
-                        gallery: {
-                            enabled: true
-                        }
-                    });
-                    function mapItems(item, index) {
-                        return {
-                            title: item.doc.site + ': ' + item.doc.date,
-                            src: '/'+scope.dbName+'/'+item.doc.images[0].path
-                        };
-                    }
-                });
-            }
+            link: magnificPopupLink
         };
     })
     .directive('imagesLoaded', function() {
         return {
             restrict: 'A',
             scope: true,
-            link: function(scope, element, attrs) {
-                attrs.$observe('imagesLoaded', function() {
-                    imagesLoaded(element, function() {
-                        $(element).masonry('layout')
-                            .find('img').addClass('loaded');
-                        attrs.$set('imagesLoadedBusy', false);
-                    });
-
-                });
-            }
+            link: imagesLoadedLink
         };
     });
 
-
-
 function ctrlThumbs($scope, cornercouch, $log, $routeParams, ejsResource) {
+    var ejs          = ejsResource('http://localhost:9200');
 
+    $scope.searchNoHits = false;
+    $scope.qs        = '';
     $scope.appending = true;
-    $scope.rows = [];
-    $scope.server = cornercouch();
-    $scope.tumblr = $scope.server.getDB($scope.dbName || 'tumblr');
+    $scope.rows      = [];
+    $scope.start     = 0;
+    $scope.server    = cornercouch();
+    $scope.tumblr    = $scope.server.getDB($scope.dbName || 'tumblr');
 
-    /* instantiate (takes an optional url string) */
-    var ejs = ejsResource('http://localhost:9200');
-
-    var query = ejs.Request()
+    $scope.query = ejs.Request()
         .indices("pvt2")
-        .query(ejs.MatchAllQuery())
         .size(100)
         .facet(ejs.TermsFacet('site')
             .field('site')
@@ -78,33 +46,73 @@ function ctrlThumbs($scope, cornercouch, $log, $routeParams, ejsResource) {
             .field('tags')
             .size(20));
 
-    var search = query.doSearch();
+    allQuery();
 
-    search.then(setFacets);
-    search.then(appendRows);
+    function allQuery() {
+        $scope.query
+            .query(ejs.MatchAllQuery())
+            .doSearch()
+            .then(appendRows);
+    }
 
     $scope.facets = {};
-    function setFacets(results) {
-        $scope.facets = results.facets;
-    }
 
     function appendRows(results) {
-        var newRows = _(results.hits.hits || []).pluck('_source');
-        $scope.rows = $scope.rows.concat(newRows);
-        setTimeout(function() {
-            $scope.appending = false;
-        }, 1000);
+        if (result.hits.total) {
+            $scope.facets = results.facets;
+            var newRows = _(results.hits.hits || []).pluck('_source');
+            $scope.rows = $scope.rows.concat(newRows);
+            setTimeout(function() {
+                $scope.appending = false;
+            }, 1000);
+        } else {
+            $scope.searchNoHits = true;
+        }
+
     }
+
+    $scope.search = function(qs) {
+        if (qs.length) {
+            $scope.qs = angular.copy(qs);
+            $scope.rows = [];
+            $scope.query = $scope.query.query(ejs.QueryStringQuery($scope.qs));
+
+            $scope.query.doSearch()
+            .then(appendRows);
+        }
+    };
 
     $scope.loadNext = function() {
         if (!$scope.appending) {
             $scope.appending = true;
             var cnt = $scope.rows.length - 1; 
-            query.from(cnt)
+            $scope.query.from(cnt)
                 .doSearch()
                 .then(appendRows);
         }
     };
-
 }
+
 //ctrlThumbs.$inject = ['$scope', '$routeParams'];
+
+function magnificPopupLink(scope, element, attrs) {
+    scope.$watch('rows', function() {
+        $(element).magnificPopup({
+            type: 'image',
+            verticalFit: false,
+            delegate: 'a',
+            preload: [1,2],
+            gallery: {
+                enabled: true
+            }
+        });
+    });
+}
+
+function imagesLoadedLink(scope, element, attrs) {
+    attrs.$observe('imagesLoaded', function() {
+        imagesLoaded(element, function() {
+            $(element).masonry('layout');
+        });
+    });
+}
