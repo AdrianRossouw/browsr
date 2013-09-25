fc7d884bbf5cc3244d846d638e0628e809912ad0
Adrian Rossouw <adrian@daemon.co.za>
2013-09-25 03:30:11 +0200

    new filter for replication.

--- couch-app/app.js
+++ couch-app/app.js
@@ -1,15 +1,24 @@
 var couchapp = require('couchapp'),
-    mime = require('couchapp/mimetypes'),
-    path = require('path');
+mime = require('couchapp/mimetypes'),
+path = require('path');
 
 mime.types.woff = 'application/x-font-woff';
 
 var ddoc = {
-  _id: '_design/app',
-  rewrites: require('./rewrites.json'),
-  views: {},
-  lists: {},
-  shows: {}
+    _id: '_design/app',
+    rewrites: require('./rewrites.json'),
+    filters: {
+        noDesign: function(doc, req) {
+            if (doc._id === '_design/app') {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    },
+    views: {},
+    lists: {},
+    shows: {}
 };
 
 couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
