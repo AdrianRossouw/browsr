111f06bfff7dd2db50927f66185d7918c8d8ed3c
Adrian Rossouw <adrian@daemon.co.za>
2013-10-25 22:34:35 +0200

    some view changes.

--- couch-app/app.js
+++ couch-app/app.js
@@ -16,7 +16,41 @@ var ddoc = {
             }
         }
     },
-    views: {},
+    views: {
+        missing: {
+            map: function(doc) {
+                if (doc.images && !doc._attachments) {
+                    return emit(doc._id + '?_rev=' + doc._rev, doc._id);
+                }
+                if (doc.images && doc._attachments) {
+                    var keys = doc.images.map(function(img) {
+                        return img.maxWidth+"/"+img.fileno;
+                    });
+                    keys.map(function(key) {
+                        if (!doc._attachments[key]) {  emit(doc._id + '?_rev=' + doc._rev, key); }
+                    });
+                }
+            }
+        },
+        wrongsize: {
+            map: function(doc) {
+                var extract = /_([0-9])*\..{3}$/;
+                if (doc.images) {
+                    doc.images.forEach(function(img) {
+                        if (img.maxWidth === 1280) {
+                            var matches = extract.exec(img.url);
+                            var urlSize = parseInt(matches[1], 10);
+
+                            if ((urlSize === 500) && (img.width > 500)) {
+                                emit(doc._id, img);
+                            }
+                        }
+                    });
+                }
+            }
+
+        }
+    },
     lists: {},
     shows: {}
 };
